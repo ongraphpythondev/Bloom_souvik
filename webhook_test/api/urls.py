be504309526e69c379ab34693b8fc0e1833e589f
pythondev1 <pythondev@ongraph.com>
2023-06-05 17:57:31 +0530

    change setting

--- webhook_test/api/urls.py
+++ webhook_test/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Authorize
 
 urlpatterns=[
-    path('authorize/',Authorize.as_view(),name="Authorize")
+    path('authorize',Authorize.as_view(),name="authorize")
 ]
